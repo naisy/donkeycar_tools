7bcc9474b501d06bc3ea16192bcc07043ad13e25
naisy <takanashi@gclue.jp>
2021-08-18 16:11:45 +0900

    support tensorrt8

---
+++ convert_h5_to_engine.py
@@ -0,0 +1,31 @@
+# Donkey Car 3
+# python ~/projects/donkeycar/scripts/freeze_model.py --model=linear.h5 --output=linear.pb
+# python /usr/lib/python3.6/dist-packages/uff/bin/convert_to_uff.py linear.pb
+# python manage.py drive --model=linear.uff --type=tensorrt_linear
+
+# Donkey Car 4
+#https://github.com/onnx/tensorflow-onnx
+#pip install -U tf2onnx
+#python convert_h5_to_engine.py
+
+########################################
+# h5 to onnx
+########################################
+import tensorflow as tf
+import onnx
+import tf2onnx.convert
+from tensorflow.keras.models import Model, load_model
+model = load_model('linear.h5')
+#model.save('linear.pb')
+
+onnx_model, _ = tf2onnx.convert.from_keras(model)
+onnx.save(onnx_model, 'linear.onnx')
+
+
+########################################
+# onnx to engine (sh command)
+########################################
+import os
+command = f'/usr/src/tensorrt/bin/trtexec --onnx=linear.onnx --saveEngine=linear.engine --fp16 --shapes=img_in:1x120x160x3 --explicitBatch'
+os.system(command)
+
